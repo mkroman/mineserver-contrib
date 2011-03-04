2e4d92b03dc592824726de3a6e86505c5a4f7437
Mikkel Kroman <mk@maero.dk>
2011-03-04 16:15:10 +0100

    Add logtype.h

--- plugins/waypoints/waypoints.cpp
+++ plugins/waypoints/waypoints.cpp
@@ -2,6 +2,7 @@
 
 #define MINESERVER_C_API
 #include "plugin_api.h"
+#include "logtype.h"
 
 typedef mineserver_pointer_struct mineserver_t;
 
@@ -10,7 +11,7 @@ mineserver_t* p_mineserver;
 PLUGIN_API_EXPORT void CALLCONVERSION waypoints_init(mineserver_t* mineserver)
 {
 	p_mineserver = mineserver;
-	p_mineserver->logger.log(6, "plugin.commands", "initialized waypoints plugin");
+	p_mineserver->logger.log(LOG_INFO, "plugin.waypoints", "initialized waypoints plugin");
 }
 
 PLUGIN_API_EXPORT void CALLCONVERSION waypoints_shutdown(void)
